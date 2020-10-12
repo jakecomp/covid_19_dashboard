4977785953105f3bc6e3fdcf145f81cf6a872b70
jakob valen <jakobvalen@jakobs-MacBook-Air.local>
2020-10-12 13:30:07 -0700

    Used bootstrap templates from bootswatch.com to create the styling desired for the dashboard application

--- covid_dash_app.py
+++ covid_dash_app.py
@@ -3,6 +3,7 @@ pd.options.plotting.backend = 'plotly'
 import dash
 import dash_core_components as dcc
 import dash_html_components as html
+import dash_bootstrap_components as dbc
 from dash.dependencies import Input,Output
 import plotly.express as px
 import plotly.io as pio
@@ -10,8 +11,9 @@ pio.renderers.default = 'firefox'
 import json
 import numpy as np
 
-app = dash.Dash(__name__)
+app = dash.Dash(__name__,external_stylesheets=[dbc.themes.CYBORG])
 
+# Limited data set, doesnt provide active/recovered/hospitalized cases by HA
 input_file = pd.read_csv('http://www.bccdc.ca/Health-Info-Site/Documents/BCCDC_COVID19_Dashboard_Case_Details.csv')
 
 input_file = input_file.rename(columns={"Sex":"Daily_Cases"})
@@ -41,21 +43,41 @@ fig = px.choropleth_mapbox(ha_total_cases,
                            hover_name = 'HA',
                            hover_data = ['Daily_Cases'],
                            mapbox_style = 'carto-darkmatter',
-                           center = {'lat':54.5,'lon':-128},
+                           center = {'lat':52.5,'lon':-128},
                            zoom = 4,
                            opacity = 0.6)
 
+fig.update_layout(autosize = True,width = 700,height=500,paper_bgcolor = 'black')
+
 Cases_by_date = input_file.groupby('Reported_Date')['Daily_Cases'].count()
 
+fig2 = Cases_by_date.plot.bar(title = 'Reported Cases')
 
 app.layout = html.Div([
 
-    html.H1("Jakob's BC Covid-19 Dashbaord",style={'text-align':'center'}),
+    dbc.Row(dbc.Col(html.H1("Jakob's BC Covid-19 Dashboard"),
+                    width={'size':'auto','offset':2}),),
 
-    html.Br(),
+    dbc.Row(dbc.Col(html.Br())),
+
+    dbc.Row(
+        [
+            dbc.Col(dcc.Graph(id = 'bc_heat_map',figure = fig),
+                width={'size':5,'offset':0,'order':11}),
+
+            dbc.Col(dcc.Graph(id = 'total_cases',figure = fig2),
+                width={'size':7,'offset':0,'order':'first'})
+        ]
+    )
+
+    #html.H1("Jakob's BC Covid-19 Dashbaord",style={'text-align':'center'}),
+
+    #html.Br(),
 
 
-    dcc.Graph(id='bc_heat_map',figure = fig)
+    #dcc.Graph(id='bc_heat_map',figure = fig),
+    #dcc.Graph(id='total_cases',figure = fig2)
+    #dcc.Graph(id='total_cases',figure = fig2)
 
 ])
 
